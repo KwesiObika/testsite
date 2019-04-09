1f284039255516e044e14a7a05df8aa8f3f44459
KwesiObika <Kwesi.Obika@colorado.edu>
2019-04-09 15:25:42 -0600

    word

--- src/components/PhotoAlbum/PhotoAlbum.js
+++ src/components/PhotoAlbum/PhotoAlbum.js
@@ -5,75 +5,125 @@ import * as countActions from '../../redux/actions/countActions.js';
 import * as albumActions from '../../redux/actions/albumActions.js';
 import * as loginActions from '../../redux/actions/loginActions.js';
 import '../../styles/photoAlbum.css';
-class Album extends Component {
-  
-  
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      width:  800,
-      height: 182
-    }
-    var screen = window.innerWidth;
-    console.log("screen size: ", screen);
-  }
-  updateDimensions() {
-    if(window.innerWidth < 500) {
-      this.setState({ width: 450, height: 102 });
-    } else {
-      let update_width  = window.innerWidth-100;
-      let update_height = Math.round(update_width/4.4);
-      this.setState({ width: update_width, height: update_height });
-    }
+import ReactDOM from 'react-dom'; 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+//global fuctions 
+//Global classes
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAPNK3FveldY5UuKLfeHNHFRJLRoG8Zu68",
+  authDomain: "picturestore-48230.firebaseapp.com",
+  databaseURL: "https://picturestore-48230.firebaseio.com",
+  projectId: "picturestore-48230",
+  storageBucket: "picturestore-48230.appspot.com",
+  messagingSenderId: "359048494620"
+};
+var fire = firebase.initializeApp(config);
+
+
+function Image(props){
+    return (
+      <button className="image" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
 
-  /**
-   * Add event listener
-   */
-  componentDidMount() {
-    this.updateDimensions();
-    window.addEventListener("resize", this.updateDimensions.bind(this));
+class Album extends React.Component {
+  state = {
+      status: 'Next player: O',
+      images: Array(9).fill(null),
+      xIsNext: true,
+      picture: null,
+      url: ''   
   }
-
-  /**
-   * Remove event listener
-   */
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions.bind(this));
+  handleClick(i){
+    const upImages = this.state.images.slice();
+    upImages[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      images: upImages,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  renderImage(i) {
+    return ( 
+      <Image    
+        value = {this.state.images[i]} 
+        onClick={() => this.handleClick(i)} 
+      />
+    );
+  }
+  fileSelectHandler = event => {
+    if(event.target.files[0]){
+      const pict = event.target.files[0];
+      this.setState({picture: pict});
+    }
+  }
+  componentWillMount(){
+   /* Create reference to messages in Firebase Database */
+   let pics = fire.database().ref('Pictures').orderByKey().limitToLast(100);
+   pics.on('child_added', snapshot => {
+     /* Update React state when message is added at Firebase Database */
+     let pics = { text: snapshot.val(), id: snapshot.key };
+     this.setState({ picture: [pics].concat(this.state.picture) });
+   })
+  }
+  upLoadImage = () => {
+    fire.database().ref('Pictures').push( this.state.picture ); // <- Send the message
+    this.setState({
+      picture: null
+    }); // <- clear the input
   }
   render() {
     return (
-      <div id ="name">
-        <h1> Album </h1>
-        <div id ="album">
-          <table>
-            <tr>
-              <td> a </td><td> b </td><td> c </td><td> d </td>
-            </tr>
-            <tr>
-              <td> e </td><td> f </td><td> g </td><td> h </td>
-            </tr>
-            <tr>
-              <td> i </td><td> j </td><td> k </td><td> l </td>
-            </tr>
-            <tr>
-              <td> m </td><td> n </td><td> o </td><td> p </td>
-            </tr>
-            <tr>
-              <td> q </td><td> r </td><td> s </td><td> t </td>
-            </tr>
-          </table>
+      <div>
+        <div className="status">{this.state.xIsNext ? 'X' : 'O'}'s Turn</div>
+        <div className="board-row">
+          {this.renderImage(0)}
+          {this.renderImage(1)}
+          {this.renderImage(2)}
         </div>
-        <br/>
-        <button onClick={this.props.albumActions.fetchMembers}>
-          Upload Image
+        <div className="board-row">
+          {this.renderImage(3)}
+          {this.renderImage(4)}
+          {this.renderImage(5)}
+        </div>
+        <div className="board-row">
+          {this.renderImage(6)}
+          {this.renderImage(7)}
+          {this.renderImage(8)}
+        </div>
+        <input type="file" onChange={this.fileSelectHandler}/>
+        <button onClick={this.upLoadImage}> upLoadImage
         </button>
       </div>
-
     );
   }
 }
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-album">
+          <Album />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
 
 const mapStateToProps = state => {
   return {
@@ -95,3 +145,5 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Album)
+
+
