ec67007be5978a163a747f70fe25c85d0ef58861
KwesiObika <Kwesi.Obika@colorado.edu>
2019-04-16 15:16:58 -0600

    new stuff

--- src/components/PhotoAlbum/PhotoAlbum.js
+++ src/components/PhotoAlbum/PhotoAlbum.js
@@ -9,6 +9,7 @@ import ReactDOM from 'react-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 //global fuctions 
 //Global classes
 // Initialize Firebase
@@ -57,29 +58,66 @@ class Album extends React.Component {
   }
   fileSelectHandler = event => {
     if(event.target.files[0]){
-      const pict = event.target.files[0].name;
-      this.setState({picture: pict});
+      console.log("Hello World");
+      console.log(event.target.files[0]);
+      const picture = event.target.files[0];
+      this.setState(() => ({picture}));
     }
   }
-  componentWillMount(){
-   /* Create reference to messages in Firebase Database */
-   let pics = fire.database().ref('Pictures').orderByKey().limitToLast(100);
-   pics.on('child_added', snapshot => {
-     /* Update React state when message is added at Firebase Database */
-     let pics = { text: snapshot.val(), id: snapshot.key };
-     this.setState({ picture: [pics].concat(this.state.picture) });
-   })
-  }
+  // componentWillMount(){
+  //  /* Create reference to messages in Firebase Database */
+  //  let pics = fire.database().ref('Pictures').orderByKey().limitToLast(100);
+  //  pics.on('child_added', snapshot => {
+  //    /* Update React state when message is added at Firebase Database */
+  //    let pics = { text: snapshot.val(), id: snapshot.key };
+  //    this.setState({ picture: [pics].concat(this.state.picture) });
+  //  })
+  // }
   upLoadImage = () => {
-    fire.database().ref('Pictures').push(this.state.picture); // <- Send the message
-    this.setState({
-      picture: null
-    }); // <- clear the input
+    console.log(this.state.picture.name);
+    const {picture} = this.state;
+    const uploadTask = fire.storage().ref('Pictures/' + this.state.picture.name).put(picture);
+    uploadTask.on('state_changed',
+      function progress(snapshot){
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+        }
+      },
+      function error(err) {
+        switch (err.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      function complete() {
+        // Upload completed successfully, now we can get the download URL
+        uploadTask.snapshot.ref.getDownloadURL().then(url => {
+          console.log('File available at', url);
+          this.setState({url});
+        });
+      }
+    );
   }
+  //var userPic = fire.storage().ref().child('Pictures/' + this.state.picture.name);
   render() {
     return (
       <div>
-        <div className="status">{this.state.xIsNext ? 'X' : 'O'}'s Turn</div>
+        <div className="status">{this.state.xIsNext ? 'X' : 'O'}s Turn</div>
+        <img src = {this.state.url} alt ="hello" width = 1200 height = 1200 />
         <div className="board-row">
           {this.renderImage(0)}
           {this.renderImage(1)}
@@ -98,6 +136,8 @@ class Album extends React.Component {
         <input type="file" onChange={this.fileSelectHandler}/>
         <button onClick={this.upLoadImage}> upLoadImage
         </button>
+        <br/>
+        
       </div>
     );
   }
